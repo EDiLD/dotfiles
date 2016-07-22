2e8678656c87f8869d82969c3936ee3b147323f8
edisz <eduardszoecs@gmail.com>
2016-07-22 15:09:53 +0200

    more dependencies

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -196,9 +196,11 @@ main() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    [ -d /usr/share/fonts/opentype ] || sudo mkdir /usr/share/fonts/opentype
-    sudo git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp
-    sudo fc-cache -f -v
+    
+    execute \
+        "[ -d /usr/share/fonts/opentype ] || sudo mkdir -p /usr/share/fonts/opentype" \
+        "sudo git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp" \
+        "sudo fc-cache -f -v"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 

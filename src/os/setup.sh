9aed016cb91b59039c7147be21ba69359a7c0c19
edisz <eduardszoecs@gmail.com>
2017-04-10 18:39:08 +0200

    new private notebook

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -3,8 +3,8 @@
 declare -r GITHUB_REPOSITORY="EDiLD/dotfiles"
 
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
-declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
-declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/src/os/utils.sh"
+declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/private"
+declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/private/src/os/utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 

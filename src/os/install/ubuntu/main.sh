d65c5673f3d89780db7ca604a64de7d99dba3dde
EDiLD <eduardszoecs@gmail.com>
2017-05-19 11:22:01 +0200

    sqlitebrowser

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -202,6 +202,10 @@ main() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    install_package "sqlitebrowser" "sqlitebrowser"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     add_ppa "webupd8team/sublime-text-3"
     update
     install_package "sublime-text-installer" "sublime-text-installer"
